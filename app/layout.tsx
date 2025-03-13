348c04e04082e1f413ead1ccccc3372306e83756
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-03-13 17:50:45 +0900

    added, configured next-auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import CategoriesBar from "./components/CategoriesBar";
+import SessionWrapper from "./components/SessionWrapper";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +27,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        <CategoriesBar />
-        {children}
+        <SessionWrapper>
+          <Navbar />
+          <CategoriesBar />
+          {children}
+        </SessionWrapper>
+        <Toaster position="top-center" />
       </body>
     </html>
   );

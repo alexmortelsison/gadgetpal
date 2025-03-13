f579f5f652a00e84318da18bde0e252d068074c8
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-03-13 17:55:41 +0900

    added signin function

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,15 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { CircleUserRound, HeartIcon, ShoppingCart } from "lucide-react";
+import { getServerSession } from "next-auth";
+import { signIn, useSession } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -27,6 +36,7 @@ const navlinks = [
   },
 ];
 export default function Navbar() {
+  const { data: session } = useSession();
   const pathName = usePathname();
   return (
     <nav className="bg-accent-foreground py-8 lg:py-10 md:py-4 lg:px-[330px] px-4 justify-between flex">
@@ -51,7 +61,26 @@ export default function Navbar() {
           </Link>
         ))}
         <div className="text-white items-center flex space-x-4 ml-12">
-          <CircleUserRound />
+          {session ? (
+            <Image
+              src={session.user?.image || ""}
+              alt="user avatar"
+              width={20}
+              height={20}
+            />
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <CircleUserRound className="cursor-pointer" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="flex items-center justify-center">
+                <Button variant={"ghost"} onClick={() => signIn("google")}>
+                  Sign in
+                </Button>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+
           <HeartIcon />
           <ShoppingCart />
         </div>
